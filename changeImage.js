da6e908a3a55cd1866c085b5ed28db309b6ddd21
Georgi Stoilkov <gogost6@gmail.com>
2022-04-29 13:14:52 +0300

    change image functionality commit

---
+++ changeImage.js
@@ -0,0 +1,16 @@
+const previewContainer = document.querySelector(".preview-container");
+const mainImg = document.querySelector(".main-img");
+
+previewContainer.addEventListener('click', function(e) {
+    let el = e.target;
+    let index;
+    if(el.dataset.index) {
+        index = el.dataset.index;
+    } else if (el.className === "preview-img-wrapper") {
+        index = el.children[0].dataset.index;
+    } else {
+        index = 6;
+    }
+
+    mainImg.src = `./assets/Sony ${index}.png`;
+});
