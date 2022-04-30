061ea417a49636f9256b25e8a5875c76dc5b95ec
Georgi Stoilkov <gogost6@gmail.com>
2022-04-30 17:11:00 +0300

    change img iprovement

--- changeImage.js
+++ changeImage.js
@@ -1,17 +1,5 @@
-const previewContainer = document.querySelector(".preview-container");
 const mainImg = document.querySelector(".main-img");
 
-document.addEventListener('click', function(e) {
-    let el = e.target;
-    let index;
-
-    if(el.dataset.index) {
-        index = el.dataset.index;
-    } else if (el.className === "preview-img-wrapper") {
-        index = el.children[0].dataset.index;
-    } else {
-        index = 6;
-    }
-
-    mainImg.src = `./assets/Sony ${index}.png`;
-});
+const change = src => {
+    mainImg.src = src
+}
