c7f8a2a2a9ec6f819025ec48806a724f95af449f
Georgi Stoilkov <gogost6@gmail.com>
2022-05-02 14:57:15 +0300

    color hover works (needs optimisation)

--- js/colorHover.js
+++ js/colorHover.js
@@ -3,30 +3,105 @@ const unionBlack = document.querySelector(".union.black");
 const unionGray = document.querySelector(".union.gray");
 const unionBlue = document.querySelector(".union.blue");
 
-choseColor.addEventListener("mouseover", function (e) {
+const colorImgWrapper = document.querySelectorAll('.color-img-wrapper');
+
+choseColor.addEventListener("click", function (e) {
     const color = e.target.dataset.color;
 
-    if(!color) {
-        unionBlack.style.display = "none";
-    }
-
     if (color === "black") {
-        unionBlack.style.display = "block";
-        unionBlue.style.display = "none";
-        unionGray.style.display = "none";
+        if (unionBlack.className === 'union black') {
+            unionBlack.style.display = "block";
+            unionBlack.classList.add('clicked');
+            unionBlue.style.display = "none";
+            unionBlue.classList.remove('clicked');
+            unionGray.style.display = "none";
+            unionGray.classList.remove('clicked');
+        } else {
+            unionBlack.style.display = "none";
+            unionBlack.classList.remove('clicked');
+            unionBlue.style.display = "none";
+            unionGray.style.display = "none";
+        }
     } else if (color === "gray") {
-        unionGray.style.display = "block";
-        unionBlack.style.display = "none";
-        unionBlue.style.display = "none";
+        if (unionGray.className === 'union gray') {
+            unionGray.style.display = "block";
+            unionGray.classList.add('clicked');
+            unionBlack.style.display = "none";
+            unionBlack.classList.remove('clicked');
+            unionBlue.style.display = "none";
+            unionBlue.classList.remove('clicked');
+        } else {
+            unionGray.style.display = "none";
+            unionGray.classList.remove('clicked');
+            unionBlack.style.display = "none";
+            unionBlue.style.display = "none";
+        }
     } else if (color === "blue") {
-        unionBlue.style.display = "block";
-        unionBlack.style.display = "none";
-        unionGray.style.display = "none";
+        if (unionBlue.className === 'union blue') {
+            unionBlue.style.display = "block";
+            unionBlue.classList.add('clicked');
+            unionBlack.style.display = "none";
+            unionBlack.classList.remove('clicked');
+            unionGray.style.display = "none";
+            unionGray.classList.remove('clicked');
+        } else {
+            unionBlue.style.display = "none";
+            unionBlue.classList.remove('clicked');
+            unionBlack.style.display = "none";
+            unionGray.style.display = "none";
+        }
     }
 });
 
-choseColor.addEventListener("mouseleave", function () {
-    unionBlack.style.display = "none";
-    unionBlue.style.display = "none";
-    unionGray.style.display = "none";
+const isClicked = (str) => str.includes('clicked');
+
+choseColor.addEventListener("mouseover", function (e) {
+    const color = e.target.dataset.color;
+
+    if (color === "black") {
+        unionBlack.style.display = "block";
+        if(!isClicked(unionGray.className)) {
+            unionGray.style.display = 'none';
+        }
+
+        if (!isClicked(unionGray.className)) {
+            unionGray.style.display = 'none';
+        }
+    }
+
+    if (color === "gray") {
+        unionGray.style.display = "block";
+        if (!isClicked(unionBlack.className)) {
+            unionBlack.style.display = 'none';
+        }
+
+        if (!isClicked(unionBlue.className)) {
+            unionBlue.style.display = 'none';
+        }
+    }
+
+    if (color === "blue") {
+        unionBlue.style.display = "block";
+        if (!isClicked(unionBlack.className)) {
+            unionBlack.style.display = 'none';
+        }
+
+        if (!isClicked(unionGray.className)) {
+            unionGray.style.display = 'none';
+        }
+    }
+});
+
+choseColor.addEventListener("mouseleave", function (e) {
+    if (unionBlack.className !== 'union black clicked') {
+        unionBlack.style.display = "none";
+    }
+
+    if (unionBlue.className !== 'union blue clicked') {
+        unionBlue.style.display = "none";
+    }
+
+    if (unionGray.className !== 'union gray clicked') {
+        unionGray.style.display = "none";
+    }
 });
